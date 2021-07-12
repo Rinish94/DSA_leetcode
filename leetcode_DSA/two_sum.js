733ad1f5b8e1c2d100b9a29fa364dd3c97bd330e
Ashutosh Pandey <rojha2030@gmail.com>
2021-07-12 21:54:37 +0530

    .

---
+++ leetcode_DSA/two_sum.js
@@ -0,0 +1,23 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+ var twoSum = function(nums, target) {
+    var pairs = {};
+     
+     for (var i = 0; i < nums.length; i++) {
+         var complement = target - nums[i];
+         if (pairs[complement] !== undefined && pairs[complement] !== i) {
+             return [pairs[complement], i];
+         }
+         pairs[nums[i]] = i;
+     }
+ };
+ 
+ 
